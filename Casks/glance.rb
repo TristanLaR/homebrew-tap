114ee2c6795ec9dbda8fbc8667feebdf182effc5
Tristan La Rusic <tristan@larusic.com>
2026-01-18 21:44:58 -0400

    Update SHA256 for v0.1.0 release

--- Casks/glance.rb
+++ Casks/glance.rb
@@ -1,6 +1,6 @@
 cask "glance" do
   version "0.1.0"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "a9e302d427d5fc846edf76d89a361d2f8c1502707ec4294b775fdf6b4b1af582"
 
   url "https://github.com/TristanLaR/glance/releases/download/v#{version}/glance-macos.tar.gz"
   name "Glance"
